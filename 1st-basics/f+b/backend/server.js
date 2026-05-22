1d7aeb2cefef4c3107a4eda6a10233a2a59e1179
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-05-22 11:01:45 +0530

    learned about CORS policy and how to resolve it

---
+++ 1st-basics/f+b/backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+const app = express();
+
+const jokes = [
+  {
+    id: 1,
+    title: "A joke 1",
+    content:
+      "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  },
+  {
+    id: 2,
+    title: "A joke 2",
+    content:
+      "Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    id: 3,
+    title: "A joke 3",
+    content:
+      "Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    id: 4,
+    title: "A joke 4",
+    content: "Why did the bicycle fall over? Because it was two tired!",
+  },
+  {
+    id: 5,
+    title: "A joke 5",
+    content:
+      "Why did the math book look sad? Because it had too many problems!",
+  },
+];
+
+app.get("/api/jokes", (req, res) => {
+  res.send(jokes);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
--- 1st-basics/f+b/frontend/src/App.jsx
+++ 1st-basics/f+b/frontend/src/App.jsx
@@ -1,12 +1,36 @@
-
+import axios from 'axios'
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.error('Error fetching jokes:', error);
+    })
+  })
 
   return (
     <>
-      
+      <h1>My Joke App</h1>
+      <p>JOKES : {jokes.length}</p>
+
+      {
+        jokes.map((joke) =>(
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+
     </>
   )
 }
